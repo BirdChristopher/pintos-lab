aac10b563f6e1180f5e78f75635a158211ecb91c
BirdChristopher <1104802845@qq.com>
2023-06-07 00:24:20 +0800

    init

---
+++ src/utils/conf.sh
@@ -0,0 +1,8 @@
+sudo cp backtrace /usr/bin
+sudo cp pintos /usr/bin
+sudo cp pintos-gdb /usr/bin
+sudo cp pintos-mkdisk /usr/bin
+sudo cp pintos-set-cmdline /usr/bin
+sudo cp Pintos.pm /usr/bin
+cd ../misc
+sudo cp gdb-macros /usr/bin
